a8fb4c30d7af7016798e20e62218695c61d3e213
schultz1 <schultz1@iastate.edu>
2020-10-18 19:18:48 -0500

    cleanup

--- getMeta.js
+++ getMeta.js
@@ -11,16 +11,11 @@ const puppeteer = require('puppeteer');
     page.waitForNavigation({ waitUntil: 'load' });
     const metaName = process.argv[2];
     await page.select("select.quick-fill-select", metaName);
-    await page.screenshot({ path: 'screenshot.png' });
 
     const out = await page.evaluateHandle(() => {
-        //document.querySelector(".poke.multi").querySelector(".quick-fill-select").querySelector("option[value=" + metaName + "]").click();
-        //await page.click("option[value=" + metaName + "]");
-
         var rankingsContainer = document.querySelector('.rankings-container');
 
         let parsedRankings = [];
-
         rankingsContainer.childNodes.forEach((ranking) => {
             let parsedRanking = new Object();
             parsedRanking.name = ranking.querySelector(".name").textContent;
@@ -31,8 +26,7 @@ const puppeteer = require('puppeteer');
         return JSON.stringify(parsedRankings);
     });
 
-    console.log(out);
-
+    await console.log(out);
 
     await browser.close();
 })();
