8293abd7b2b000d89f29e0fb9b8b9f95dd193f0b
schultz1 <schultz1@iastate.edu>
2020-10-18 19:07:38 -0500

    get meta into object array

--- getMeta.js
+++ getMeta.js
@@ -6,12 +6,34 @@ const puppeteer = require('puppeteer');
     const page = await browser.newPage();
     await page.goto('http://localhost/pvpoke/src/battle/');
 
+    await page.select("select.mode-select", "matrix");
 
-    var metaName = process.argv[2];
-    document.querySelector(".poke.multi").querySelector(".quick-fill-select").querySelector("option[value=" + metaName + "]").click();
+    page.waitForNavigation({ waitUntil: 'load' });
+    const metaName = process.argv[2];
+    await page.select("select.quick-fill-select", metaName);
+    await page.screenshot({ path: 'screenshot.png' });
 
-    var rankingsContainer = document.querySelector('.rankings-container');
-    console.log(JSON.stringify(rankingsContainer));
+    const out = await page.evaluateHandle(() => {
+        //document.querySelector(".poke.multi").querySelector(".quick-fill-select").querySelector("option[value=" + metaName + "]").click();
+        //await page.click("option[value=" + metaName + "]");
+
+        var rankingsContainer = document.querySelector('.rankings-container');
+
+        let parsedRankings = [];
+
+        rankingsContainer.childNodes.forEach((ranking) => {
+            let parsedRanking = new Object();
+            parsedRanking.name = ranking.querySelector(".name").textContent;
+            parsedRanking.movesString = ranking.querySelector(".moves").textContent;
+            parsedRankings.push(parsedRanking);
+        });
+
+        return JSON.stringify(parsedRankings);
+    });
+
+    console.log(out);
+
 
     await browser.close();
 })();
+
