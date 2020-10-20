39d641d3e13cbfa70b947161103670bef8d8ba05
schultz1 <schultz1@iastate.edu>
2020-10-19 20:17:41 -0500

    write meta to a temp file

--- getMeta.js
+++ getMeta.js
@@ -1,5 +1,6 @@
 
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 
 (async () => {
     const browser = await puppeteer.launch();
@@ -11,6 +12,11 @@ const puppeteer = require('puppeteer');
     page.waitForNavigation({ waitUntil: 'load' });
     const metaName = process.argv[2];
     await page.select("select.quick-fill-select", metaName);
+
+    // for whatever reason, this works to wait for a page load,
+    // but nothing else will...
+    await page.screenshot({ path: 'test.png' });
+
 
     const out = await page.evaluateHandle(() => {
         var rankingsContainer = document.querySelector('.rankings-container');
@@ -26,7 +32,11 @@ const puppeteer = require('puppeteer');
         return JSON.stringify(parsedRankings);
     });
 
-    await console.log(out);
+
+    //await console.log(out._remoteObject.value);
+    fs.writeFile('./temp/meta.txt', out._remoteObject.value, (err) => {
+        if (err) throw err;
+    });
 
     await browser.close();
 })();
