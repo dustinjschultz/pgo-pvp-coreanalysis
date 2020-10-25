6e733a83624745847d892aa8acb0d8944f732eb5
schultz1 <schultz1@iastate.edu>
2020-10-25 15:02:32 -0500

    get logic fully stubbed

--- mapMoves.js
+++ mapMoves.js
@@ -6,6 +6,22 @@ const fs = require('fs');
 var browser;
 var page;
 
+function getMoveNumbersFromPage(thePokemonEntry) {
+    var myMoveSelectors = document.querySelectorAll(`div.modal-content > div.poke .move-select-container > .move-select`);
+    var myReturn = "";
+    myReturn += getMoveFromSelectNode(myMoveSelectors[0], thePokemonEntry.fastMove);
+    myReturn += "-";
+    myReturn += getMoveFromSelectNode(moMoveSelectors[1], thePokemonEntry.chargedMoves[0]);
+    myReturn += "-";
+    myReturn += getMoveFromSelectNode(moMoveSelectors[1], thePokemonEntry.chargedMoves[1]);
+    return myReturn;
+}
+
+function getMoveFromSelectNode(theSelectNode, theChargeMoveId) {
+    var myChild = theSelectNode.querySelector(`option[value="${theChargeMoveId}"`);
+    return Array.prototype.indexOf.call(myChild.parent.children, myChild);
+}
+
 fs.readFile('./temp/meta.txt', 'utf8', function (err, data) {
     (async () => {
         //browser = await puppeteer.launch();
@@ -13,7 +29,10 @@ fs.readFile('./temp/meta.txt', 'utf8', function (err, data) {
         page = await browser.newPage();
 
         var meta = JSON.parse(data);
+        await setUpPage();
+
         await getMonsMoveNumbersString(meta);
+        console.log(meta);
 
         fs.writeFile('./temp/metaWithNumbers.txt', JSON.stringify(meta), (err) => {
             if (err) throw err;
@@ -30,32 +49,30 @@ async function getMonsMoveNumbersString(thePokemonList) {
     }
 }
 
-async function getMonMoveNumbersString(thePokemonEntry) {
-    var metaName = process.argv[2];
-
+async function setUpPage() {
     await page.goto("http://localhost/pvpoke/src/team-builder");
-    page.waitForNavigation({ waitUntil: 'networkidle2' }) 
+    page.waitForNavigation({ waitUntil: 'networkidle2' });
+    await page.addScriptTag({ content: `${getMoveNumbersFromPage}` });
+    await page.addScriptTag({ content: `${getMoveFromSelectNode}` });
 
+    var metaName = process.argv[2];
     await page.evaluate(optionSelector => {
         return document.querySelector(optionSelector).setAttribute('selected', 'true');
     }, `select.format-select > option[cup="${metaName}"]`);
-    console.log('blah');
 
-    //await page.waitForNavigation({ waitUntil: 'networkidle2' });
+    await page.$eval(`button.add-poke-btn`, el => el.click());
+}
 
-    //await page.$eval(`button.add-poke-btn`, el => el.click());
-    //await page.waitForNavigation({ waitUntil: 'networkidle2' });
+async function getMonMoveNumbersString(thePokemonEntry) {
+    await page.evaluate(optionSelector => {
+        return document.querySelector(optionSelector).setAttribute('selected', 'true');
+    }, `div.modal-content > div.poke > select.poke-select > option[value="${thePokemonEntry.speciesId}"]`);
 
-    //await page.select("select.poke-select", thePokemonEntry.speciesId);
-    //await page.waitForNavigation({ waitUntil: 'networkidle2' });
+    var myMoveNumbersString = await getMoveNumbers(thePokemonEntry);
+    thePokemonEntry.moveNumbersString = myMoveNumbersString;
 
 }
 
-function getFastMoveNumberFromPage(theFastMoveId) {
+function getMoveNumbers(thePokemonEntry) {
 
 }
-
-function getChargeMoveNumberFromPage(theChargeMoveId) {
-
-}
-
