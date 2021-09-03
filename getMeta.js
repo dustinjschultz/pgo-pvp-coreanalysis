6aa0f3e34c909b5f4add111f1fb69bce3ed27f10
schultz1 <schultz1@iastate.edu>
2021-09-03 15:51:30 -0500

    use dotenv for urls and install paths

--- getMeta.js
+++ getMeta.js
@@ -1,8 +1,9 @@
 
 const fs = require('fs');
 const metaName = process.argv[2];
+require('dotenv').config()
 
-fs.readFile(`C:/xampp/htdocs/pvpoke/src/data/groups/${metaName}.json`, 'utf8', function (err, data) {
+fs.readFile(`${process.env.PVPOKE_SRC_LOCATION}data/groups/${metaName}.json`, 'utf8', function (err, data) {
     fs.writeFile('./temp/meta.txt', data, (err) => {
         if (err) throw err;
     });
