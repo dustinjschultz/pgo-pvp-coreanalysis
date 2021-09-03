1cd90ba17b087696f99554cf9d4f624b83a3fa35
schultz1 <schultz1@iastate.edu>
2021-09-03 16:19:26 -0500

    use env for the current run config

--- getMeta.js
+++ getMeta.js
@@ -1,7 +1,7 @@
 
 const fs = require('fs');
-const metaName = process.argv[2];
 require('dotenv').config()
+const metaName = process.env.RUN_META_NAME;
 
 fs.readFile(`${process.env.PVPOKE_SRC_LOCATION}data/groups/${metaName}.json`, 'utf8', function (err, data) {
     fs.writeFile('./temp/meta.txt', data, (err) => {
