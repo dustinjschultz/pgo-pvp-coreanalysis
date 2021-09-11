a71ec4bcafffcd6c21577b671b10f1723ca2a5e3
schultz1 <schultz1@iastate.edu>
2021-09-11 15:17:01 -0500

    partially define runData

---
+++ runData.js
@@ -0,0 +1,30 @@
+
+// TODO: define a graphData which is an array of Nodes and array of Edges
+// TODO: define Node
+// TODO: define Edge
+// TODO: figure out how to do the above since they're anonymous types
+
+
+class MonData {
+    speciesId = "";
+    fastMove = "";
+    chargedMoves = []; // String array
+    moveNumbersString = "";
+
+    constructor(speciesId, fastMove, chargedMoves, moveNumbersString) {
+        this.speciesId = speciesId;
+        this.fastMove = fastMove;
+        this.chargedMoves = chargedMoves;
+        this.moveNumbersString = moveNumbersString;
+
+    }
+}
+
+class RunData {
+    mons = []; // array of MonData
+    metaCoverageArray = [[]]; // String array array
+
+    constructor(mons) {
+        this.mons = mons;
+    }
+}
