662e63d021cfcc96b26548d8258b682528644515
schultz1 <schultz1@iastate.edu>
2021-10-15 13:18:17 -0500

    add fromAnonObject methods

--- runData.js
+++ runData.js
@@ -10,7 +10,17 @@ class MonData {
         this.fastMove = fastMove;
         this.chargedMoves = chargedMoves;
         this.moveNumbersString = moveNumbersString;
+    }
 
+    static fromAnonObject(anonObject) {
+        var myReturn = new MonData(
+            anonObject.speciesId,
+            anonObject.fastMove,
+            anonObject.chargedMoves,
+            anonObject.moveNumbersString
+        );
+
+        return myReturn;
     }
 }
 
@@ -29,4 +39,24 @@ class RunData {
         }
         return myReturn;
     }
+
+    getJson() {
+        return JSON.stringify(this);
+    }
+
+    static fromAnonObject(anonObject) {
+        var myReturn = new RunData([]);
+
+        for (var anonMon of anonObject.mons) {
+            var myMonData = MonData.fromAnonObject(anonMon);
+            myReturn.mons.push(myMonData);
+        }
+
+        myReturn.metaCoverageArray = anonObject.metaCoverageArray;
+        return myReturn;
+        // potential enhancement: using Object.keys(myAnonObject) to make it dynamic
+        // need to figure out how to deal with objects with nested keys though
+    }
 }
+
+// TODO: next steps: actually use this Class
